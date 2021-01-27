97bd48920856d82181af3fcd41546e6afc6ff7d4
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-27 21:26:54 +0600

    1-When-to-use-a-function-function-inside-an-array-4 added

---
+++ 1-When-to-use-a-function-function-inside-an-array-4.js
@@ -0,0 +1,20 @@
+function enenify_all(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        enenify(num);
+        if (num % 2 == 0) {
+            console.log(num, ': is even number');
+        } else {
+            console.log(num * 2, ': is odd number');
+        }
+    }
+}
+nums = [5, 12, 89, 45, 18, 8];
+enenify_all(nums);
+
+console.log('---------------------------------');
+
+friends_age = [13, 17, 19, 20, 18];
+enenify_all(friends_age);
+
+console.log('---------------------------------');
