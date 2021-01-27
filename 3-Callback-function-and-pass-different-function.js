257eef7dc61aaf819b1b310bb9c7663ec8f428a6
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-28 00:02:36 +0600

    3-Callback-function-and-pass-different-function updated

--- 3-Callback-function-and-pass-different-function.js
+++ 3-Callback-function-and-pass-different-function.js
@@ -11,5 +11,12 @@ function washHand() {
 function taskShower() {
     console.log('Take Shower');
 }
-explain('sagir', 20, washHand);
-explain('galib', 27, taskShower);
+
+function scrollFacebook() {
+    console.log('scroll Facebook but do not like post');
+}
+explain('Sagir Uddin', 20, washHand);
+console.log('---------------------------')
+explain('Galib Uddin', 27, taskShower);
+console.log('---------------------------')
+explain('Mogir Uddin', 25, scrollFacebook);
