2230b9b6140888c3fde4f4ffe87ea99db2005ac8
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-28 10:29:31 +0600

    5-How-to-organize-code-inside-a-function added

---
+++ 5-How-to-organize-code-inside-a-function.js
@@ -0,0 +1,8 @@
+/*
+1. variable ke sobar aage upore declare kore dite hobe
+
+2. variable sudu ekbar use hole oi jaigai sudu use kora jaite pare
+
+2. function ke o read ableility thik rakhe call korlte hobe
+
+*/
