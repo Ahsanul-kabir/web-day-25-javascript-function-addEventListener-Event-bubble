1f1adcc9d1a4008dcd77fb2b15026e142986bb64
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-28 10:56:13 +0600

    6-What-is-Event-different-types-of-event-in-web added

---
+++ 6-What-is-Event-different-types-of-event-in-web.js
@@ -0,0 +1,8 @@
+/*
+1. Javascript Event likes keybord er up , down, left, right, etc.
+2. Javascript er Event likes Mouse diya doubleClick, scroll, etc.
+
+::: tar men hoitese kisu ekta gotanoi holo Event
+*/
+
+// learn::: https://developer.mozilla.org/en-US/docs/Web/Events
